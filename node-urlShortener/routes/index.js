d59888fcf3273dab314ea735be9371ab522e53c5
ButterflyInfusion <106527566+ButterflyInfusion@users.noreply.github.com>
2022-06-07 11:03:43 +0300

    Delete node-urlShortener directory

--- node-urlShortener/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const url = require('../models/Url');
-
-//welcome page
-router.get('/', (req, res)=> res.render('welcome'))
-
-
-module.exports = router
